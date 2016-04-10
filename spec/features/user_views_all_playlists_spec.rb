96bd59ced9b4a3f535f050cbf31aac559552ae0a
Sal Espinosa <sespinos@gmail.com>
2016-04-10 11:26:15 -0600

    complete iteration 4

--- spec/features/user_views_all_playlists_spec.rb
+++ spec/features/user_views_all_playlists_spec.rb
@@ -1,5 +1,16 @@
-# As a user
-# Given that playlists exist in the database
-# When I visit the playlists index
-# Then I should see each playlist's name
-# And each name should link to that playlist's individual page
+require 'rails_helper'
+
+RSpec.feature "User views all playlists" do
+  scenario "display all playlist names and links" do
+    playlist1, playlist2, playlist3 = create_list(:playlist, 3)
+
+    visit playlists_path
+
+    within("li:first") do
+      expect(page).to have_content(playlist1.name)
+    end
+    within("li:last") do
+      expect(page).to have_link(playlist3.name, href: playlist_path(playlist3.id))
+    end
+  end
+end
--- spec/models/playlist_song_spec.rb
+++ spec/models/playlist_song_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PlaylistSong, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:playlist) }
+  it { should belong_to(:song) }
 end
